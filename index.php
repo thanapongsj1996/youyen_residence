27c0359d8adbe577996bd2afa8c3e8c638d39be9
Thanapong Somjai <thanapongsomjai@Thanapongs-MacBook-Pro.local>
2019-06-26 10:55:47 +0700

    Process bar, Our Rooms

--- index.php
+++ index.php
@@ -8,79 +8,206 @@
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Prompt">
+  <link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.min.css">
   <link rel="stylesheet" href="assets/css/style.css">
+  <script src="https://kit.fontawesome.com/56a5f7cfae.js"></script>
 
   <title>You yen Residence</title>
+  <style>
+    .progress-container {
+      width: 100%;
+      height: 8px;
+      background: rgba(0, 0, 0 , 0.6);
+    }
+
+    .progress-bar {
+      height: 8px;
+      background: rgba(217, 125, 17 , 0.3);;
+      width: 0%;
+    }
+
+  </style>
 </head>
 
 <body>
-  <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background: rgba(0, 0, 0 , 0.3)">
-    <div class="container">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Rooms</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Location</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
-          </li>
-        </ul>
+  <!-- NavBar -->
+  <div class="fixed-top">
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background: rgba(0, 0, 0 , 0.6); padding:14px">
+      <div class="container">
+        <a class="navbar-brand" href="#">You Yen Residence</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Rooms</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Location</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
       </div>
+    </nav>
+    <div class="progress-container sticky-top">
+      <div class="progress-bar" id="myBar"></div>
     </div>
-  </nav>
-  <div class="bd-example">
-  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  </div>
+
+
+
+
+  <!-- NavBar -->
+  <!-- Carousel -->
+  <section id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
-      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
     </ol>
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="https://images.unsplash.com/photo-1550883299-cbd89e6576e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2689&q=80" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <div class="carousel-img" style="background-image: url('https://images.unsplash.com/photo-1501183638710-841dd1904471?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80')">
+          <div class="carousel-caption">
+            <h1 class="display-4 font-weight-bold">You Yen Residence @ Phuket</h1>
+            <p class="lead">Welcome to Phuket, The happiness place</p>
+          </div>
+          <div class="blackscreen"></div>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="https://images.unsplash.com/photo-1550883299-cbd89e6576e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2689&q=80" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <div class="carousel-img" style="background-image: url('https://images.unsplash.com/photo-1513694203232-719a280e022f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1949&q=80')">
+          <div class="carousel-caption">
+            <h1 class="display-4 font-weight-bold">You Yen Residence @ Phuket</h1>
+            <p class="lead">Welcome to Phuket, The happiness place</p>
+          </div>
+          <div class="blackscreen"></div>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="https://images.unsplash.com/photo-1550883299-cbd89e6576e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2689&q=80" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Third slide label</h5>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        <div class="carousel-img" style="background-image: url('https://images.unsplash.com/photo-1493809842364-78817add7ffb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80')">
+          <div class="carousel-caption">
+            <h1 class="display-4 font-weight-bold">You Yen Residence @ Phuket</h1>
+            <p class="lead">Welcome to Phuket, The happiness place</p>
+          </div>
+          <div class="blackscreen"></div>
         </div>
       </div>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
     </a>
-    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="sr-only">Next</span>
     </a>
-  </div>
-</div>
+  </section>
+  <!-- Carousel -->
+  <!-- Services -->
+  <section class="container my-3 py-5">
+    <h1 class="border-short-bottom">Services</h1>
+    <div class="row text-center mt-5 mb-5">
+      <div class="col-6 col-md-4">
+        <i class="fas fa-wifi fa-6x icon-style"></i>
+        <h4 class="card-title mt-3">Free Wifi</h4>
+      </div>
+      <div class="col-6 col-md-4">
+        <i class="fas fa-wifi fa-6x icon-style"></i>
+        <h4 class="card-title mt-3">Free Wifi</h4>
+      </div>
+      <div class="col-6 col-md-4">
+        <i class="fas fa-wifi fa-6x icon-style"></i>
+        <h4 class="card-title mt-3">Free Wifi</h4>
+      </div>
+      <div class="col-6 col-md-4">
+        <i class="fas fa-wifi fa-6x icon-style"></i>
+        <h4 class="card-title mt-3">Free Wifi</h4>
+      </div>
+      <div class="col-6 col-md-4">
+        <i class="fas fa-wifi fa-6x icon-style"></i>
+        <h4 class="card-title mt-3">Free Wifi</h4>
+      </div>
+      <div class="col-6 col-md-4">
+        <i class="fas fa-wifi fa-6x icon-style"></i>
+        <h4 class="card-title mt-3">Free Wifi</h4>
+      </div>
+    </div>
+
+  </section>
+  <!-- Services -->
+  <!-- 4Images -->
+  <section>
+    <div class="row container-fluid p-0 m-0">
+      <div class="col-6 col-sm p-0"><img style="width:100%" src="https://images.unsplash.com/photo-1499933374294-4584851497cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80" /></div>
+      <div class="col-6 col-sm p-0"><img style="width:100%" src="https://images.unsplash.com/photo-1499933374294-4584851497cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80" /></div>
+      <div class="col-6 col-sm p-0"><img style="width:100%" src="https://images.unsplash.com/photo-1499933374294-4584851497cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80" /></div>
+      <div class="col-6 col-sm p-0"><img style="width:100%" src="https://images.unsplash.com/photo-1499933374294-4584851497cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80" /></div>
+    </div>
+  </section>
+  <!-- 4Images -->
+  <!-- Our Rooms -->
+  <section class="container my-3 py-5">
+    <h1 class="border-short-bottom">Our Rooms</h1>
+    <div class="row justify-content-between text-center">
+      <div class="col-12 col-md-4">
+        <div class="card mb-3">
+          <img src="https://images.unsplash.com/photo-1523755231516-e43fd2e8dca5?ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h4 class="card-title">Delux Room</h4>
+            <p class="card-text text-muted">800 Baht/Night</p>
+            <p class="card-text text-muted">8000 Baht/Month</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-4">
+        <div class="card mb-3">
+          <img src="https://images.unsplash.com/photo-1533008093099-e2681382639a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1001&q=80" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h4 class="card-title">Delux Room</h4>
+            <p class="card-text text-muted">800 Baht/Night</p>
+            <p class="card-text text-muted">8000 Baht/Month</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-4">
+        <div class="card mb-3">
+          <img src="https://images.unsplash.com/photo-1534889156217-d643df14f14a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h4 class="card-title">Delux Room</h4>
+            <p class="card-text text-muted">800 Baht/Night</p>
+            <p class="card-text text-muted">8000 Baht/Month</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+  <!-- Our Rooms -->
 
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script>
+    // When the user scrolls the page, execute myFunction 
+    window.onscroll = function() {
+      myFunction()
+    };
+
+    function myFunction() {
+      var winScroll = document.body.scrollTop || document.documentElement.scrollTop; //ความสูงจากด้านบน
+      var height = document.documentElement.scrollHeight - document.documentElement.clientHeight; //ความสูงทั้งหมด - ความสูงหน้าจอ
+      var scrolled = (winScroll / height) * 100;
+      document.getElementById("myBar").style.width = scrolled + "%";
+    }
+  </script>
 </body>
 
 </html>
